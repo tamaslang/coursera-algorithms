f0b6a5ac89421f71060955ad509d07cef6561d1d
Tamas Lang <tamas.lang@talangsoft.org>
2016-10-03 18:13:25 +0100

    Initial commit for first week merge sort

---
+++ src/main/java/org/talangsoft/algoritms/week1/MergeSort.java
@@ -0,0 +1,37 @@
+package org.talangsoft.algoritms.week1;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    private int[] leftOf(int[] array) {
+        return Arrays.copyOfRange(array, 0, array.length / 2);
+    }
+
+    private int[] rightOf(int[] array) {
+        return Arrays.copyOfRange(array, array.length / 2, array.length);
+    }
+
+    private int[] sort(int[] left, int[] right) {
+        int[] mergedOut = new int[left.length + right.length];
+        int[] sortedLeft = left.length == 1 ? left : sort(leftOf(left), rightOf(left));
+        int[] sortedRight = right.length == 1 ? right : sort(leftOf(right), rightOf(right));
+
+        for (int n = 0, i = 0, j = 0; n < mergedOut.length; n++) {
+            if (j >= sortedRight.length || (i < sortedLeft.length && sortedLeft[i] < sortedRight[j])) {
+                mergedOut[n] = sortedLeft[i++];
+            } else {
+                mergedOut[n] = sortedRight[j++];
+            }
+        }
+        return mergedOut;
+    }
+
+    public int[] mergeSort(int[] numbers) {
+        if(numbers.length < 2) {
+            return numbers;
+        } else {
+            return sort(leftOf(numbers), rightOf(numbers));
+        }
+    }
+}
---
+++ src/test/week1/MergeSortTest.java
@@ -0,0 +1,57 @@
+package week1;
+
+import org.junit.Test;
+import org.talangsoft.algoritms.week1.MergeSort;
+
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MergeSortTest {
+
+    private MergeSort underTest = new MergeSort();
+
+    int[] from1toMillion = IntStream.rangeClosed(1,1000000).toArray();
+    int[] fromMillionTo1 = IntStream.rangeClosed(1,1000000).map(nr -> 1000000 - nr +1).toArray();
+
+    @Test
+    public void testMergeSortForSortedInputWith1MillionItems() {
+        assertThat(underTest.mergeSort(from1toMillion), is(from1toMillion));
+    }
+
+    @Test
+    public void testMergeSortForUnsortedInputWith1MillionItems() {
+        assertThat(underTest.mergeSort(fromMillionTo1), is(from1toMillion));
+    }
+
+    @Test
+    public void testMergeSortForSortedInputWith2Items() {
+        assertThat(underTest.mergeSort(new int[]{1, 2}), is(new int[]{1, 2}));
+    }
+
+    @Test
+    public void testMergeSortForUnSortedInputWith2Items() {
+        assertThat(underTest.mergeSort(new int[]{2, 1}), is(new int[]{1, 2}));
+    }
+
+    @Test
+    public void testMergeSortForUnSortedInputWith4Items() {
+        assertThat(underTest.mergeSort(new int[]{4, 3, 2, 1}), is(new int[]{1, 2, 3, 4}));
+    }
+
+    @Test
+    public void testMergeSortForUnSortedInputWith5Items() {
+        assertThat(underTest.mergeSort(new int[]{5, 4, 3, 2, 1}), is(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    public void testMergeSortForInputWith1Items() {
+        assertThat(underTest.mergeSort(new int[]{1}), is(new int[]{1}));
+    }
+
+    @Test
+    public void testMergeSortForInputWithNoItems() {
+        assertThat(underTest.mergeSort(new int[]{}), is(new int[]{}));
+    }
+}
