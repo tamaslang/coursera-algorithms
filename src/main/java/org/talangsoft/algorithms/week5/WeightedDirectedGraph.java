8827e34c84ec986b49c378bc2a76584360e80132
Tamas Lang <tamas.lang@talangsoft.org>
2016-11-04 06:52:19 +0000

    week5/ first try on Dijkstra algorithm

---
+++ src/main/java/org/talangsoft/algorithms/week5/WeightedDirectedGraph.java
@@ -0,0 +1,84 @@
+package org.talangsoft.algorithms.week5;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class WeightedDirectedGraph {
+
+    private Map<Integer, int[][]> graphData;
+
+    public WeightedDirectedGraph(Map<Integer, int[][]> source) {
+        this.graphData = new HashMap<>(source.size());
+        for (Integer key : source.keySet()) {
+            int[][] adjacentsWithWeights = source.get(key);
+            int[][] copyOfAdjacentsWithWeights = new int[source.get(key).length][];
+            System.arraycopy(adjacentsWithWeights, 0, copyOfAdjacentsWithWeights, 0, copyOfAdjacentsWithWeights.length);
+            this.graphData.put(key, copyOfAdjacentsWithWeights);
+            // create empty nodes too
+            for (int[] adjacentWithWeight : copyOfAdjacentsWithWeights) {
+                if (!this.graphData.containsKey(adjacentWithWeight[0])) {
+                    this.graphData.put(adjacentWithWeight[0], new int[][]{});
+                }
+            }
+        }
+    }
+
+    public int[][] getAdjacents(Integer vertex) {
+        return graphData.get(vertex);
+    }
+
+    public Integer[] getVertices() {
+        return graphData.keySet().stream().toArray(Integer[]::new);
+    }
+
+    private Map.Entry<Integer, Integer> removeAndReturnMinimumVertex(Map<Integer, Integer> vertexToValue) {
+        Map.Entry<Integer, Integer> vertexWithMin = null;
+        for (Map.Entry<Integer, Integer> currentVertex : vertexToValue.entrySet()) {
+            if (vertexWithMin == null || currentVertex.getValue() < vertexWithMin.getValue()) {
+                vertexWithMin = currentVertex;
+            }
+        }
+        vertexToValue.remove(vertexWithMin.getKey());
+        return vertexWithMin;
+    }
+
+    public Map<Integer, Integer> shortestPathsWithDijkstra(Integer s) {
+        /**
+         *
+         *          ---@2 ---
+         *     (1)/     |     \ (6)
+         *      /       |      @
+         *   1(s)       |(2)    4
+         *      \       |      @
+         *    (4)\      @     / (3)
+         *        \____@3___/
+         *
+         */
+
+        // create Q -> unvisited nodes with endless distances except starting point
+        Map<Integer, Integer> unvisitedQ = Arrays.stream(getVertices()).collect(Collectors.toMap(vertex -> vertex, vertex -> Integer.MAX_VALUE));
+        // mark starting vertex s to 0 distance
+        unvisitedQ.put(s, 0);
+        // create V -> storage for visited nodes with distances: the solution, eventually
+        Map<Integer, Integer> visitedV = new HashMap<>(getVertices().length);
+        while (!unvisitedQ.isEmpty()) {
+            // find min in unvisited Q
+            Map.Entry<Integer, Integer> minimumVertex = removeAndReturnMinimumVertex(unvisitedQ);
+            visitedV.put(minimumVertex.getKey(), minimumVertex.getValue());
+
+            // find all neighbours that are in  Q
+            int[][] neighboursWithDistances = getAdjacents(minimumVertex.getKey());
+            // calculate distances to neighbours
+            for (int[] neighbourAndDistance : neighboursWithDistances) {
+                if (unvisitedQ.containsKey(neighbourAndDistance[0])) {
+                    unvisitedQ.put(neighbourAndDistance[0], minimumVertex.getValue() + neighbourAndDistance[1]);
+                }
+            }
+        }
+
+        return visitedV;
+    }
+
+}
--- src/test/java/org/talangsoft/algorithms/week5/WeightedGraphTest.java
+++ src/test/java/org/talangsoft/algorithms/week5/WeightedGraphTest.java
@@ -3,35 +3,43 @@ package org.talangsoft.algorithms.week5;
 import org.junit.Test;
 import org.talangsoft.algorithms.helper.InputFileReader;
 
+import java.util.Arrays;
 import java.util.Map;
 
-import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class WeightedGraphTest {
     private String weightedGraphSourceFile = "week5/example_weighted_graph_format.txt";
     private String assignmentGraphSourceFile = "week5/dijkstra_data.txt";
+    private String lectureGraphSourceFile = "week5/week5_lecture_graph.txt";
+    private String testcase1GraphSourceFile = "week5/forum_testcase1.txt";
+    private String testcase2GraphSourceFile = "week5/forum_testcase2.txt";
+    private String testcase3GraphSourceFile = "week5/forum_testcase3.txt";
 
     @Test
     public void readSourceFileWithExampleGraph() throws Exception {
         Map<Integer, int[][]> graphData = InputFileReader.readDirectedWeightedGraph(weightedGraphSourceFile);
-
-        assertThat(graphData.get(1), is(new int[][]{{3, 9}}));
-        assertThat(graphData.get(2), is(new int[][]{{1, 3}, {5, 4}}));
-        assertThat(graphData.get(3), is(new int[][]{{2, 4}, {5, 11}}));
-        assertThat(graphData.get(4), is(new int[][]{{2, 7}}));
-        assertThat(graphData.get(5), is(new int[][]{{4, 8}}));
-
         WeightedDirectedGraph graph = new WeightedDirectedGraph(graphData);
-        // TODO assert on size of vertices
+
+        assertThat(graph.getVertices().length, is(5));
+
+        assertThat(graph.getAdjacents(1), is(new int[][]{{3, 9}}));
+        assertThat(graph.getAdjacents(2), is(new int[][]{{1, 3}, {5, 4}}));
+        assertThat(graph.getAdjacents(3), is(new int[][]{{2, 4}, {5, 11}}));
+        assertThat(graph.getAdjacents(4), is(new int[][]{{2, 7}}));
+        assertThat(graph.getAdjacents(5), is(new int[][]{{4, 8}}));
     }
 
     @Test
     public void readSourceFileWithAssignmentGraph() throws Exception {
         Map<Integer, int[][]> graphData = InputFileReader.readDirectedWeightedGraph(assignmentGraphSourceFile);
 
-        assertThat(graphData.get(1), is(new int[][]{
+        WeightedDirectedGraph graph = new WeightedDirectedGraph(graphData);
+
+        assertThat(graph.getVertices().length, is(200));
+
+        assertThat(graph.getAdjacents(1), is(new int[][]{
                 {80, 982}, {163, 8164}, {170, 2620}, {145, 648},
                 {200, 8021}, {173, 2069}, {92, 647}, {26, 4122},
                 {140, 546}, {11, 1913}, {160, 6461}, {27, 7905},
@@ -41,16 +49,143 @@ public class WeightedGraphTest {
                 {125, 2909}, {117, 2597}, {55, 6399}
         }));
 
+        // find shortest paths from edge '1'
+        Map<Integer, Integer> shortestPaths = graph.shortestPathsWithDijkstra(1);
+        assertThat(shortestPaths.get(1), is(0));
+        assertThat(shortestPaths.get(200), is(4487));
+
+    }
+
+
+    @Test
+    public void solveWeek5AssignmentByCalculatingSomeDistances() throws Exception{
+        Map<Integer, int[][]> graphData = InputFileReader.readDirectedWeightedGraph(assignmentGraphSourceFile);
+
         WeightedDirectedGraph graph = new WeightedDirectedGraph(graphData);
-        // TODO assert on size of vertices
+        Map<Integer, Integer> shortestPaths = graph.shortestPathsWithDijkstra(1);
+        int[] vertices = new int[]{7,37,59,82,99,115,133,165,188,197};
+        String[] distancesForVertices = Arrays.stream(vertices).mapToObj(vertex -> shortestPaths.get(vertex).toString()).toArray(String[]::new);
+        assertThat(String.join(",",distancesForVertices), is("13374,2610,6094,10341,6765,16786,2029,2442,2505,9831"));
+
+
+    }
+
+    @Test
+    public void testcase1Graph() throws Exception{
+        Map<Integer, int[][]> graphData = InputFileReader.readDirectedWeightedGraph(testcase1GraphSourceFile);
+        WeightedDirectedGraph graph = new WeightedDirectedGraph(graphData);
+
+        Map<Integer, Integer> shortestPathFromVertex1 = graph.shortestPathsWithDijkstra(1);
+        assertThat(shortestPathFromVertex1.get(1), is(0));
+        assertThat(shortestPathFromVertex1.get(2), is(1));
+        assertThat(shortestPathFromVertex1.get(3), is(2));
+        assertThat(shortestPathFromVertex1.get(4), is(3));
+        assertThat(shortestPathFromVertex1.get(5), is(4));
+        assertThat(shortestPathFromVertex1.get(6), is(4));
+        assertThat(shortestPathFromVertex1.get(7), is(3));
+        assertThat(shortestPathFromVertex1.get(8), is(2));
+
+        /*
+        1 0 []
+        2 1 [2]
+        3 2 [2, 3]
+        4 3 [2, 3, 4]
+        5 4 [2, 3, 4, 5]
+        6 4 [8, 7, 6]
+        7 3 [8, 7]
+        8 2 [8]
+         */
+    }
+
+    @Test
+    public void testcase2Graph() throws Exception{
+        Map<Integer, int[][]> graphData = InputFileReader.readDirectedWeightedGraph(testcase2GraphSourceFile);
+        WeightedDirectedGraph graph = new WeightedDirectedGraph(graphData);
+
+        Map<Integer, Integer> shortestPathFromVertex1 = graph.shortestPathsWithDijkstra(1);
+        assertThat(shortestPathFromVertex1.get(1), is(0));
+        assertThat(shortestPathFromVertex1.get(2), is(6));
+        assertThat(shortestPathFromVertex1.get(3), is(2));
+        assertThat(shortestPathFromVertex1.get(4), is(4));
+        assertThat(shortestPathFromVertex1.get(5), is(3));
+        assertThat(shortestPathFromVertex1.get(6), is(10));
+        assertThat(shortestPathFromVertex1.get(7), is(11));
+        assertThat(shortestPathFromVertex1.get(8), is(9));
+        assertThat(shortestPathFromVertex1.get(9), is(12));
+
+        /*
+            1:0,
+            2:6,
+            3:2,
+            4:4,
+            5:3,
+            6:10,
+            7:11,
+            8:9,
+            9:12,
+         */
+    }
+
+    @Test
+    public void testcase3graph() throws Exception{
+        Map<Integer, int[][]> graphData = InputFileReader.readDirectedWeightedGraph(testcase3GraphSourceFile);
+        WeightedDirectedGraph graph = new WeightedDirectedGraph(graphData);
+
+        Map<Integer, Integer> shortestPathFromVertex1 = graph.shortestPathsWithDijkstra(1);
+        assertThat(shortestPathFromVertex1.get(1), is(0));
+        assertThat(shortestPathFromVertex1.get(2), is(3));
+        assertThat(shortestPathFromVertex1.get(3), is(2));
+        assertThat(shortestPathFromVertex1.get(4), is(6));
+        assertThat(shortestPathFromVertex1.get(5), is(9));
+        assertThat(shortestPathFromVertex1.get(6), is(4));
+        assertThat(shortestPathFromVertex1.get(7), is(4));
+        assertThat(shortestPathFromVertex1.get(8), is(6));
+        assertThat(shortestPathFromVertex1.get(9), is(8));
+        assertThat(shortestPathFromVertex1.get(10), is(4));
+
+//        To vertex: 1, length: 0
+//        To vertex: 2, length: 3
+//        To vertex: 3, length: 2
+//        To vertex: 4, length: 6
+//        To vertex: 5, length: 9
+//        To vertex: 6, length: 4
+//        To vertex: 7, length: 4
+//        To vertex: 8, length: 6
+//        To vertex: 9, length: 8
+//        To vertex: 10, length: 4
+
+
+
+    }
+
+    @Test
+    public void testWeek4ExampleGraph() throws Exception {
+        /**
+         *
+         *          ---@2 ---
+         *     (1)/     |     \ (6)
+         *      /       |      @
+         *   1(s)       |(2)    4
+         *      \       |      @
+         *    (4)\      @     / (3)
+         *        \____@3___/
+         *
+         */
+        Map<Integer, int[][]> graphData = InputFileReader.readDirectedWeightedGraph(lectureGraphSourceFile);
+        WeightedDirectedGraph graph = new WeightedDirectedGraph(graphData);
+
+        assertThat(graph.getVertices().length, is(4));
+
+        assertThat(graph.getAdjacents(1), is(new int[][]{{2, 1}, {3, 4}}));
+        assertThat(graph.getAdjacents(2), is(new int[][]{{3, 2}, {4, 6}}));
+        assertThat(graph.getAdjacents(3), is(new int[][]{{4, 3}}));
+        assertThat(graph.getAdjacents(4), is(new int[][]{}));
+
+        // calculate shortest path
+        Map<Integer, Integer> shortestPathFromVertex1 = graph.shortestPathsWithDijkstra(1);
+        assertThat(shortestPathFromVertex1.get(1), is(0));
+        assertThat(shortestPathFromVertex1.get(2), is(1));
+        assertThat(shortestPathFromVertex1.get(3), is(3));
+        assertThat(shortestPathFromVertex1.get(4), is(6));
     }
 }
-
-
-/*
-1 3,9
-2 1,3 5,4
-3 2,4 5,11
-4 2,7
-5 4,8
- */
